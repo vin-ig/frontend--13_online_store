b4b97d8ebe03938a1fe7a711cc6ca354a75f0e2d
Vinokurov Igor <vin-ig@yandex.ru>
2025-12-03 18:16:58 +0300

    Панель фильтров

--- frontend/src/app/shared/components/category-filter/category-filter.component.ts
+++ frontend/src/app/shared/components/category-filter/category-filter.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ActiveParamsType} from "../../../../types/active-params.type";
 
 @Component({
     selector: 'category-filter',
@@ -9,6 +11,11 @@ import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
 export class CategoryFilterComponent implements OnInit {
     @Input() categoryWithTypes: CategoryWithTypeType | null = null
     @Input() type: string | null = null
+    open: boolean = false
+    activeParams: ActiveParamsType = {types: []}
+
+    from: number | null = null
+    to: number | null = null
 
     get title(): string {
         if (this.categoryWithTypes) {
@@ -23,10 +30,80 @@ export class CategoryFilterComponent implements OnInit {
         return ''
     }
 
-    constructor() {
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        ) {
     }
 
     ngOnInit(): void {
+        this.activatedRoute.queryParams.subscribe(params => {
+            // I. Восстанавливаем значения фильтров из других компонентов
+            const activeParams: ActiveParamsType = {types: []}
+
+            if (params.hasOwnProperty('types')) {
+                activeParams.types = Array.isArray(params['types']) ? params['types'] : [params['types']]
+            }
+            if (params.hasOwnProperty('heightFrom')) {activeParams.heightFrom = params['heightFrom']}
+            if (params.hasOwnProperty('heightTo')) {activeParams.heightTo = params['heightTo']}
+            if (params.hasOwnProperty('diameterFrom')) {activeParams.diameterFrom = params['diameterFrom']}
+            if (params.hasOwnProperty('diameterTo')) {activeParams.diameterTo = params['diameterTo']}
+            if (params.hasOwnProperty('sort')) {activeParams.sort = params['sort']}
+            if (params.hasOwnProperty('page')) {activeParams.page = +params['page']}
+
+            this.activeParams = activeParams
+
+            // II. Восстанавливаем фильтры из url
+            if (this.type) {
+                if (this.type === 'height') {
+                    this.open = !!(this.activeParams.heightFrom || this.activeParams.heightTo)
+                    this.from = this.activeParams.heightFrom ? +this.activeParams.heightFrom : null
+                    this.to = this.activeParams.heightTo ? +this.activeParams.heightTo : null
+                } else if (this.type === 'diameter') {
+                    this.open = !!(this.activeParams.diameterFrom || this.activeParams.diameterTo)
+                    this.from = this.activeParams.diameterFrom ? +this.activeParams.diameterFrom : null
+                    this.to = this.activeParams.diameterTo ? +this.activeParams.diameterTo : null
+                }
+            } else {
+                this.activeParams.types = params['types']
+
+                if (this.categoryWithTypes && this.categoryWithTypes.types && this.categoryWithTypes.types.length > 0 &&
+                this.categoryWithTypes.types.some(type => this.activeParams.types.find(item => type.url === item))) {
+                    this.open = true
+                }
+            }
+        })
     }
 
+    toggle(): void {
+        this.open = !this.open
+    }
+
+    updateFilterParam(url: string, checked: boolean): void {
+        if (this.activeParams.types && this.activeParams.types.length > 0) {
+            const existingTypeInParams = this.activeParams.types.find(item => item === url)
+            if (existingTypeInParams && !checked) {
+                this.activeParams.types = this.activeParams.types.filter(item => item != url)
+            } else if (!existingTypeInParams && checked) {
+                // this.activeParams.types.push(url)  Angular bug
+                this.activeParams.types = [...this.activeParams.types, url]
+            }
+        } else if (checked) {
+            this.activeParams.types = [url]
+        }
+
+        this.router.navigate(['/catalog'], {queryParams: this.activeParams})
+    }
+
+    updateFilterParamFromTo(param: string, value: string): void {
+        if (param === 'heightFrom' || param === 'heightTo' || param === 'diameterFrom' || param === 'diameterTo') {
+            if (this.activeParams[param] && !value) {
+                delete this.activeParams[param]
+            } else {
+                this.activeParams[param] = value
+            }
+        }
+
+        this.router.navigate(['/catalog'], {queryParams: this.activeParams})
+    }
 }
