f48eb0e91110487ab98747f13869dd1146ae7914
Vinokurov Igor <vin-ig@yandex.ru>
2025-12-06 12:11:11 +0300

    AuthGuard

---
+++ frontend/src/app/views/personal/personal-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FavoriteComponent} from "./favorite/favorite.component";
+import {InfoComponent} from "./info/info.component";
+import {OrdersComponent} from "./orders/orders.component";
+
+const routes: Routes = [
+    {path: 'favorite', component: FavoriteComponent},
+    {path: 'orders', component: OrdersComponent},
+    {path: 'info', component: InfoComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class PersonalRoutingModule {
+}
