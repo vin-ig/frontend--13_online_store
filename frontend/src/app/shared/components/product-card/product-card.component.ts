39059a1cfec8478b55cbb0be15fb5a965d5bbfe6
Vinokurov Igor <vin-ig@yandex.ru>
2025-12-08 16:27:59 +0300

    Изменена видимость функционала "Избранное"

--- frontend/src/app/shared/components/product-card/product-card.component.ts
+++ frontend/src/app/shared/components/product-card/product-card.component.ts
@@ -18,6 +18,7 @@ import {Router} from "@angular/router";
 export class ProductCardComponent implements OnInit {
     readonly serverStaticPath: string = environment.serverStaticPath
     count: number = 1
+    isLogged: boolean = false
 
     @Input() product!: ProductType
     @Input() isLite: boolean = false
@@ -30,12 +31,17 @@ export class ProductCardComponent implements OnInit {
         private _snackBar: MatSnackBar,
         private router: Router,
     ) {
+        this.isLogged = this.authService.isLogged
     }
 
     ngOnInit(): void {
         if (this.countInCart && this.countInCart > 1) {
             this.count = this.countInCart
         }
+
+        this.authService.isLogged$.subscribe((result: boolean) => {
+            this.isLogged = result
+        })
     }
 
     updateCount(value: number): void {
